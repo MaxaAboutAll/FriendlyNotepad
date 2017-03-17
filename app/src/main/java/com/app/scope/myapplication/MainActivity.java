ddea683ef87e316b8b8fb308ed5eacdf4a6c81ef
MaxaAboutAll <maxaaboutall@gmail.com>
2017-03-17 18:53:51 +0500

    SUKA BLYAT

--- app/src/main/java/com/app/scope/myapplication/MainActivity.java
+++ app/src/main/java/com/app/scope/myapplication/MainActivity.java
@@ -139,7 +139,7 @@ public class MainActivity extends AppCompatActivity {
                 public void onDataChange(DataSnapshot dataSnapshot) {
 // This method is called once with the initial value and again
 // whenever data at this location is updated.
-                    for(int i=0;i<20;i++) {
+                    for(int i=0;i<2147483646;i++) {
                         Name.add(dataSnapshot.child("users").child(id).child("NOTES").child("Note"+(i+1)).child("ID").getValue(String.class));
                         Text.add(dataSnapshot.child("users").child(id).child("NOTES").child("Note"+(i+1)).child("Text").getValue(String.class));
                     }
